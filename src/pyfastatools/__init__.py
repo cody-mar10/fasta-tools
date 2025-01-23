6106cff7ef6ad603f27bb4c1f429af2572515717
cody-mar10 <ccmartin6@wisc.edu>
2025-01-23 15:30:51 -0600

    remove deleted file API and add header(s) to public API

--- src/pyfastatools/__init__.py
+++ src/pyfastatools/__init__.py
@@ -1,14 +1,6 @@
-from pyfastatools._fastatools import Record, Records
+from pyfastatools._fastatools import Header, Headers, Record, Records
 from pyfastatools._parser import Parser
 from pyfastatools._simple import PyParser
-from pyfastatools.file import FastaFile
 from pyfastatools.utils import write_fasta
 
-__all__ = [
-    "Parser",
-    "Record",
-    "Records",
-    "write_fasta",
-    # "FastaFile",
-    # "PyParser",
-]
+__all__ = ["Parser", "Record", "Records", "Header", "Headers", "write_fasta"]
